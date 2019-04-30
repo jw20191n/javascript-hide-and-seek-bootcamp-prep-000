e387e3b2305e19c1d0f1c76a39de9273dd5fe4a0
Jiayi Wang <clairejw1015@gmail.com>
2019-04-30 22:49:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   let rank = document.querySelectorAll('ul.ranked-list li')
   for(let i=0;i<rank.length;i++){
-    rank[i].innerHTML = i+n.parseInt();
+    rank[i].innerHTML = rank[i]
     
   } 
 }
