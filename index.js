c632ccd597a8c85819056ec426708c26471a18c7
Jiayi Wang <clairejw1015@gmail.com>
2019-04-30 22:47:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+
+}
+
+function increaseRankBy(n){
+  let rank = document.querySelector('ul.ranked-list li')
+}
